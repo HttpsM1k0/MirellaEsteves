6c7622d4513f5a1dfa0eb88697b4a2adf25066c9
mirellaarantes <mirella.arantes@escola.pr.gov.br>
2024-10-16 09:29:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'gótica',
+    'O que significa ser gótico?',
+    'Os góticos são pessoas atraídas pelo mórbido e que convivem com o que a psicologia transcendental chama de sombra da personalidade, que são aspectos ocultos e escuros do indivíduo. Está relacionado com a tristeza e o sadismo.'
+)
+
+criaCartao(
+    'Gyaru',
+    'O que é uma gyaru?',
+    '“Gyaru” é a forma japonesa de se pronunciar a palavra “Girl” or “Gal”,e refere-se a um padrão estético compartilhado por meninas e mulheres japonesas, que inclui tanto adolescentes quanto mulheres na faixa dos 30,embora seja mais comum até os 25 anos de idade.'
+)
+
+criaCartao(
+    'emo',
+    'O que é o estilo emo?',
+    'Consideram-se pessoas altamente emotivas e sensíveis às letras das músicas de sua preferência, as quais têm como temática a melancolia, a tristeza, problemas que envolvem a temática do amor, da rejeição do outro (como da própria família).'
+)
+
+criaCartao(
+    'cutecore',
+    'Como que é o estilo cutecore?',
+    'Geralmente pessoas que seguem esse estilo usam laços no cabelo. Esse estilo lembra muito o "Kawaii", mas sendo um pouco mais discreto por não ser tão colorido.'
+)
